2b9b41b45dbe4a88c37b06e545b082c0f8631aba
Christina Raganit <craganit@my.bcit.ca>
2020-11-24 19:21:43 -0800

    resources added, navigation bar added but not fully functional for some reason idk why

--- app/src/main/java/ca/bcit/vividhealth/HomeActivity.java
+++ app/src/main/java/ca/bcit/vividhealth/HomeActivity.java
@@ -1,16 +1,24 @@
 package ca.bcit.vividhealth;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidUserException;
@@ -18,7 +26,7 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener  {
 
     private FirebaseAuth mAuth;
     private Object FirebaseAuthInvalidUserException;
@@ -61,14 +69,71 @@ public class HomeActivity extends AppCompatActivity {
                     }
                 });
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawer,
+                toolbar,
+                R.string.nav_open_drawer,
+                R.string.nav_close_drawer);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
 
-
-
-
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
         // Firebase Auth
 
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+        Fragment fragment = null;
+        Intent intent = null;
+
+        switch(id) {
+            case R.id.nav_home:
+                intent = new Intent(this, HomeActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_profile:
+                intent = new Intent(this, ProfileActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_reminders:
+//                intent = new Intent(this, ReminderActivity.class);
+                break;
+            case R.id.nav_workouts:
+//                intent = new Intent(this, WorkoutActivity.class);
+                break;
+            case R.id.nav_about:
+//                intent = new Intent(this, AboutActivity.class);
+                break;
+            case R.id.nav_feedback:
+//                intent = new Intent(this, FeedbackActivity.class);
+                break;
+            case R.id.nav_toggle_mode:
+                break;
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     public void onNewReminder(View v) {
