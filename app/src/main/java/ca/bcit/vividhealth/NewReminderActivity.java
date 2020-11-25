08ff4463ea8cb5b106d0fed580102c27b6222edd
Christina Raganit <craganit@my.bcit.ca>
2020-11-24 22:18:13 -0800

    attempt to implement add reminder to db

--- app/src/main/java/ca/bcit/vividhealth/NewReminderActivity.java
+++ app/src/main/java/ca/bcit/vividhealth/NewReminderActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class NewReminderActivity extends AppCompatActivity {
@@ -16,7 +18,14 @@ public class NewReminderActivity extends AppCompatActivity {
     }
 
     public void onCreateNewReminder(View v) {
-
+        EditText new_reminder_title = findViewById(R.id.new_reminder_title);
+        RadioButton radioButtonWeek = findViewById(R.id.radioButtonWeek);
+        RadioButton radioButton3Days = findViewById(R.id.radioButton3Days);
+        RadioButton radioButtonDay = findViewById(R.id.radioButtonDay);
+        RadioButton radioButton12Hours = findViewById(R.id.radioButton12Hours);
+        RadioButton radioButton3Hours = findViewById(R.id.radioButton3Hours);
+        EditText editTextTimeBegin = findViewById(R.id.editTextTimeBegin);
+        EditText editTextTimeEnd = findViewById(R.id.editTextTimeEnd);
 
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
