4c9e43350ddab11990b5d5ed3ab6c4515a69520f
Christina Raganit <craganit@my.bcit.ca>
2020-11-28 19:37:35 -0800

    About us page added, just edit your guys' descriptions :D

---
+++ app/src/main/java/ca/bcit/vividhealth/AboutUs.java
@@ -0,0 +1,134 @@
+package ca.bcit.vividhealth;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.net.Uri;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class AboutUs extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawer,
+                toolbar,
+                R.string.nav_open_drawer,
+                R.string.nav_close_drawer);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        drawer.bringToFront();
+        drawer.requestLayout();
+
+        Button chris_github = findViewById(R.id.chris_github_btn);
+        Button asher_github = findViewById(R.id.asher_github_btn);
+        Button zoltan_github = findViewById(R.id.zoltan_github_btn);
+
+        chris_github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://github.com/christinaraganit"));
+                startActivity(intent);
+            }
+        });
+
+        asher_github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://github.com/AsherGum"));
+                startActivity(intent);
+            }
+        });
+
+        zoltan_github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://github.com/zoltanbi"));
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+        Fragment fragment = null;
+        Intent intent = null;
+
+        switch(id) {
+            case R.id.nav_home:
+                intent = new Intent(this, HomeActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_profile:
+                intent = new Intent(this, ProfileActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_reminders:
+                intent = new Intent(this, NewReminderActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_workouts:
+                intent = new Intent(this, PresetWorkouts.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_about:
+                intent = new Intent(this, AboutUs.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_feedback:
+//                intent = new Intent(this, FeedbackActivity.class);
+                break;
+            case R.id.nav_toggle_mode:
+                break;
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+}
