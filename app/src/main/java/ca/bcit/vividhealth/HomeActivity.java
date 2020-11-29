ace90c58a90e7cbd3ef7ad2d9df623f98e5a26a6
Christina Raganit <craganit@my.bcit.ca>
2020-11-29 01:08:12 -0800

    pls no merge errors pls

--- app/src/main/java/ca/bcit/vividhealth/HomeActivity.java
+++ app/src/main/java/ca/bcit/vividhealth/HomeActivity.java
@@ -95,18 +95,79 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         setSupportActionBar(toolbar);
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        SharedPreferences sharedPreferences
+                = getSharedPreferences(
+                "sharedPrefs", MODE_PRIVATE);
+        final SharedPreferences.Editor editor
+                = sharedPreferences.edit();
+        final boolean isDarkModeOn
+                = sharedPreferences
+                .getBoolean(
+                        "isDarkModeOn", false);
+
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        MenuItem menuItem1 = navigationView.getMenu().findItem(R.id.nav_toggle_mode); // This is the menu item that contains your switch
+        final Switch drawerSwitch = (Switch) menuItem1.getActionView().findViewById(R.id.drawer_switch);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this,
                 drawer,
                 toolbar,
                 R.string.nav_open_drawer,
                 R.string.nav_close_drawer);
+
         drawer.addDrawerListener(toggle);
+        drawer.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+            }
+
+            @Override
+            public void onDrawerOpened(@NonNull View drawerView) {
+                if (isDarkModeOn) {
+                    drawerSwitch.setChecked(true);
+                    AppCompatDelegate
+                            .setDefaultNightMode(
+                                    AppCompatDelegate
+                                            .MODE_NIGHT_YES);
+                }
+                else {
+                    drawerSwitch.setChecked(false);
+                    AppCompatDelegate
+                            .setDefaultNightMode(
+                                    AppCompatDelegate
+                                            .MODE_NIGHT_NO);
+                }
+            }
+
+            @Override
+            public void onDrawerClosed(@NonNull View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        });
         toggle.syncState();
+        if (isDarkModeOn) {
+            drawerSwitch.setChecked(true);
+            AppCompatDelegate
+                    .setDefaultNightMode(
+                            AppCompatDelegate
+                                    .MODE_NIGHT_YES);
+        }
+        else {
+            drawerSwitch.setChecked(false);
+            AppCompatDelegate
+                    .setDefaultNightMode(
+                            AppCompatDelegate
+                                    .MODE_NIGHT_NO);
+        }
 
-
-        navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
         // Firebase Auth
 
         // Placing user reminders on the home screen
@@ -133,11 +194,59 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 //            }
 //        });
 
+        Log.d("giraffe", "What's up! 1");
+
         mToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.enabled, R.string.disabled);
         drawer.addDrawerListener(mToggle);
         drawer.setClickable(true);
         mToggle.setDrawerIndicatorEnabled(true);
         mToggle.syncState();
+
+        // THE CODE THAT WORKS :D
+//        MenuItem menuItem1 = navigationView.getMenu().findItem(R.id.nav_toggle_mode); // This is the menu item that contains your switch
+//        Switch drawerSwitch = (Switch) menuItem1.getActionView().findViewById(R.id.drawer_switch);
+//        drawerSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if (isChecked) {
+//                    Toast.makeText(HomeActivity.this, "Switch turned on", Toast.LENGTH_SHORT).show();
+//                    AppCompatDelegate
+//                            .setDefaultNightMode(
+//                                    AppCompatDelegate
+//                                            .MODE_NIGHT_YES);
+//                } else {
+//                    Toast.makeText(HomeActivity.this, "Switch turned off", Toast.LENGTH_SHORT).show();
+//                    AppCompatDelegate
+//                            .setDefaultNightMode(
+//                                    AppCompatDelegate
+//                                            .MODE_NIGHT_NO);
+//                }
+//            }
+//        });
+
+
+        drawerSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    AppCompatDelegate
+                            .setDefaultNightMode(
+                                    AppCompatDelegate
+                                            .MODE_NIGHT_YES);
+                    editor.putBoolean(
+                            "isDarkModeOn", true);
+                    editor.apply();
+                } else {
+                    AppCompatDelegate
+                            .setDefaultNightMode(
+                                    AppCompatDelegate
+                                            .MODE_NIGHT_NO);
+                    editor.putBoolean(
+                            "isDarkModeOn", false);
+                    editor.apply();
+                }
+            }
+        });
     }
 
     @Override
@@ -167,6 +276,8 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         Fragment fragment = null;
         Intent intent = null;
 
+        Log.d("giraffe", "id: "+ id + " nav_toggle id: " + R.id.nav_toggle_mode);
+
         switch (id) {
             case R.id.nav_home:
                 System.out.println("Home clicked");
@@ -187,26 +298,13 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
                 startActivity(intent);
                 break;
             case R.id.nav_about:
+                Log.d("giraffe", "What's up!4");
                 intent = new Intent(this, AboutUs.class);
                 startActivity(intent);
                 break;
             case R.id.nav_feedback:
                 intent = new Intent(this, SendFeedback.class);
                 startActivity(intent);
-                break;
-            case R.id.nav_toggle_mode:
-                MenuItem menuItem1 = navigationView.getMenu().findItem(R.id.nav_toggle_mode); // This is the menu item that contains your switch
-                Switch drawerSwitch = (Switch) menuItem1.getActionView().findViewById(R.id.drawer_switch);
-                drawerSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                    @Override
-                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                        if (isChecked) {
-                            Toast.makeText(HomeActivity.this, "Switch turned on", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(HomeActivity.this, "Switch turned off", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
                 break;
         }
 
--- app/src/main/java/ca/bcit/vividhealth/SendFeedback.java
+++ app/src/main/java/ca/bcit/vividhealth/SendFeedback.java
@@ -3,6 +3,7 @@ package ca.bcit.vividhealth;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -10,16 +11,22 @@ import androidx.fragment.app.Fragment;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
 public class SendFeedback extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private ActionBarDrawerToggle mToggle;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,21 +58,149 @@ public class SendFeedback extends AppCompatActivity implements NavigationView.On
         setSupportActionBar(toolbar);
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        SharedPreferences sharedPreferences
+                = getSharedPreferences(
+                "sharedPrefs", MODE_PRIVATE);
+        final SharedPreferences.Editor editor
+                = sharedPreferences.edit();
+        final boolean isDarkModeOn
+                = sharedPreferences
+                .getBoolean(
+                        "isDarkModeOn", false);
+
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        MenuItem menuItem1 = navigationView.getMenu().findItem(R.id.nav_toggle_mode); // This is the menu item that contains your switch
+        final Switch drawerSwitch = (Switch) menuItem1.getActionView().findViewById(R.id.drawer_switch);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this,
                 drawer,
                 toolbar,
                 R.string.nav_open_drawer,
                 R.string.nav_close_drawer);
+
         drawer.addDrawerListener(toggle);
+        drawer.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+                if (isDarkModeOn) {
+                    drawerSwitch.setChecked(true);
+                    AppCompatDelegate
+                            .setDefaultNightMode(
+                                    AppCompatDelegate
+                                            .MODE_NIGHT_YES);
+                }
+                else {
+                    drawerSwitch.setChecked(false);
+                    AppCompatDelegate
+                            .setDefaultNightMode(
+                                    AppCompatDelegate
+                                            .MODE_NIGHT_NO);
+                }
+            }
+
+            @Override
+            public void onDrawerOpened(@NonNull View drawerView) {
+            }
+
+            @Override
+            public void onDrawerClosed(@NonNull View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        });
+        if (isDarkModeOn) {
+            drawerSwitch.setChecked(true);
+            AppCompatDelegate
+                    .setDefaultNightMode(
+                            AppCompatDelegate
+                                    .MODE_NIGHT_YES);
+        }
+        else {
+            drawerSwitch.setChecked(false);
+            AppCompatDelegate
+                    .setDefaultNightMode(
+                            AppCompatDelegate
+                                    .MODE_NIGHT_NO);
+        }
         toggle.syncState();
-
-
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
 
         drawer.bringToFront();
         drawer.requestLayout();
+
+//
+//        buttonToggleDark.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if (isChecked) {
+//                    AppCompatDelegate
+//                            .setDefaultNightMode(
+//                                    AppCompatDelegate
+//                                            .MODE_NIGHT_YES);
+//                } else {
+//                    AppCompatDelegate
+//                            .setDefaultNightMode(
+//                                    AppCompatDelegate
+//                                            .MODE_NIGHT_NO);
+//                }
+//            }
+//        });
+
+        mToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.enabled, R.string.disabled);
+        drawer.addDrawerListener(mToggle);
+        drawer.setClickable(true);
+        mToggle.setDrawerIndicatorEnabled(true);
+        mToggle.syncState();
+
+        // THE CODE THAT WORKS :D
+//        MenuItem menuItem1 = navigationView.getMenu().findItem(R.id.nav_toggle_mode); // This is the menu item that contains your switch
+//        Switch drawerSwitch = (Switch) menuItem1.getActionView().findViewById(R.id.drawer_switch);
+//        drawerSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if (isChecked) {
+//                    Toast.makeText(HomeActivity.this, "Switch turned on", Toast.LENGTH_SHORT).show();
+//                    AppCompatDelegate
+//                            .setDefaultNightMode(
+//                                    AppCompatDelegate
+//                                            .MODE_NIGHT_YES);
+//                } else {
+//                    Toast.makeText(HomeActivity.this, "Switch turned off", Toast.LENGTH_SHORT).show();
+//                    AppCompatDelegate
+//                            .setDefaultNightMode(
+//                                    AppCompatDelegate
+//                                            .MODE_NIGHT_NO);
+//                }
+//            }
+//        });
+
+
+        drawerSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    AppCompatDelegate
+                            .setDefaultNightMode(
+                                    AppCompatDelegate
+                                            .MODE_NIGHT_YES);
+                    editor.putBoolean(
+                            "isDarkModeOn", true);
+                    editor.apply();
+                } else {
+                    AppCompatDelegate
+                            .setDefaultNightMode(
+                                    AppCompatDelegate
+                                            .MODE_NIGHT_NO);
+                    editor.putBoolean(
+                            "isDarkModeOn", false);
+                    editor.apply();
+                }
+            }
+        });
     }
 
     @Override
